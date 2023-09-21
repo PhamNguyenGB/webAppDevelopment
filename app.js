33b244f623b8c14bb2c582e174c45792efa23d6d
Pham Nguyen Gia Bao <phambao091@gmail.com>
2023-09-21 20:57:45 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const contactRouter = require('./app/routes/contact.route');
+const ApiError = require('./app/api_error');
 
 const app = express();
 
@@ -8,11 +9,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 app.get('/', (req, res) => {
     res.json({ message: "welcome to contact book application"});
 });
 
 app.use('/api/contacts', contactRouter);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, 'Resource Not Found'));
+});
+
+app.use((req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || 'Internal Server Error',
+    });
+});
 
 module.exports = app;
