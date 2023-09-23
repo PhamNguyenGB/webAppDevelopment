a9b3a8e17deb54d6935dce27278767e18e038efc
Pham Nguyen Gia Bao <phambao091@gmail.com>
2023-09-23 14:21:22 +0700

    update code

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
     return next(new ApiError(404, 'Resource Not Found'));
 });
 
-app.use((req, res, next) => {
+app.use((error, req, res, next) => {
     return res.status(error.statusCode || 500).json({
         message: error.message || 'Internal Server Error',
     });
